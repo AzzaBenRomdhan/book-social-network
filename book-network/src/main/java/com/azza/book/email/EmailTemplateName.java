c9458b50e8bd96aaa3be6149a4374b39c7ef01d2
AzzaBenRomdhan <benromdhan.azza99@gmail.com>
2025-02-12 16:37:01 +0100

    first commit

---
+++ book-network/src/main/java/com/azza/book/email/EmailTemplateName.java
@@ -0,0 +1,15 @@
+package com.azza.book.email;
+
+import lombok.Getter;
+
+public enum EmailTemplateName {
+
+    ACTIVATE_ACCOUNT("activate_account")
+    ;
+
+
+    private final String name;
+    EmailTemplateName(String name) {
+        this.name = name;
+    }
+}
---
+++ book-network/src/main/java/com/azza/book/user/TokenRepository.java
@@ -0,0 +1,13 @@
+package com.azza.book.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+    Optional<Token> findByToken(String token);
+    @Query("SELECT t FROM Token t")
+    List<Token> findAllTokens();
+}

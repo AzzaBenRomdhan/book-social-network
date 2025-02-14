aad0cd5a89fc355cae91175f380316d3434cde81
AzzaBenRomdhan <benromdhan.azza99@gmail.com>
2025-02-14 18:05:22 +0100

    OpenApi documentation config

--- book-network/src/main/java/com/azza/book/book/Book.java
+++ book-network/src/main/java/com/azza/book/book/Book.java
@@ -34,4 +34,15 @@ public class Book extends BaseEntity {
     private List <FeedBack> feedbacks;
     @OneToMany(mappedBy = "book")
     private List <BookTransactionHistory> histories;
+    @Transient
+    public double getRate() {
+        if(feedbacks == null || feedbacks.isEmpty()) {
+            return 0.0;
+        }
+        var rate = this.feedbacks.stream()
+                .mapToDouble(FeedBack::getNote)
+                .average()
+                .orElse(0.0);
+        return Math.round(rate * 10.0) / 10.0;
+    }
 }
